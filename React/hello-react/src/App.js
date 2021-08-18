e9b44765397069b1c63a3ccefd0315f06b8a32c5
ghkddyto <dyto153@naver.com>
2021-08-18 17:21:54 +0900

    react

---
+++ React/hello-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  var name = '황요새';
+
+  return (
+    <div className='text'>
+      안녕 리액트
+    </div>
+  );
+}
+
+export default App;
