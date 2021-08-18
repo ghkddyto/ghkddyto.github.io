3d28b54db794293abe5f960a9a97d1aa9a21dbc1
ghkddyto <dyto153@naver.com>
2021-08-18 19:05:01 +0900

    1-day

--- Study-React/whant/src/App.js
+++ Study-React/whant/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+
       <header className='App-header'>
         <a href='#' className='home'>
           <img src='/header/home.png' alt='' />
@@ -12,6 +13,56 @@ function App() {
           <img src='/header/wish.png' alt='' />
         </a>
       </header>
+
+      <body>
+
+        <div className='categories'>
+          <div className='icon'>
+            <img src='/logo/filter.png' alt='' />
+          </div>
+          <div className='cat_wrap'>
+            <a href='#' className='cat cat1'>
+              <img src='/logo/넷플.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat2'>
+              <img src='/logo/왓챠.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat3'>
+              <img src='/logo/웨이브.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat4'>
+              <img src='/logo/티빙.png' alt='' />
+            </a>
+            <a href='#' className='cat cat5'>
+              <img src='/logo/쿠팡.png' alt='' />
+            </a>
+            <a href='#' className='cat cat6'>
+              <img src='/logo/구글 플레이.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat7'>
+              <img src='/logo/네이버 시리즈.png' alt='' />
+            </a>
+            <a href='#' className='cat cat8'>
+              <img src='/logo/디즈니+.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat9'>
+              <img src='/logo/U+.png' alt='' />
+            </a>
+            <a href='#' className='cat cat10'>
+              <img src='/logo/라프텔.jpg' alt='' />
+            </a>
+            <a href='#' className='cat cat11'>
+              <img src='/logo/시즌.png' alt='' />
+            </a>
+            <a href='#' className='cat cat12'>
+             <img src='/logo/카카오페이지.png' alt='' />
+            </a>
+          </div>
+        </div>
+
+
+      </body>
+
     </div>
   );
 }
